f49dfae02898f5a15d2b8729ee0d49f739a6d93f
LinVivian61 <linvivian61@gmail.com>
2022-02-04 18:03:19 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const path = require('path')
+
+const app = express();
+
+app.use(express.static("public"))
+
+app.get(['/', '/hw0.html'], (req, res) => {
+  res.sendFile(path.join(__dirname, '../../','hw0.html'))
+} )
+
+app.listen(5000, () => console.log('server started on port 5000'))
