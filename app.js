782c64efb2b8f76fa85157dac8d4163cbf40ee53
LinVivian61 <linvivian61@gmail.com>
2022-02-04 19:08:53 +0000

    add html

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static("public"))
 
 app.get(['/', '/hw0.html'], (req, res) => {
-  res.sendFile(path.join(__dirname, '../../','hw0.html'))
+  res.sendFile(path.join(__dirname, '/public','hw0.html'))
 } )
 
 app.listen(5000, () => console.log('server started on port 5000'))
